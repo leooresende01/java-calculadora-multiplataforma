0030d8fe85a12600064e670178a8870eed837367
leooresende01 <leosousa1211@gmail.com>
2022-09-07 18:49:33 -0300

    Continuando visual da calculadora

--- src/main/java/tk/leooresende01/calculadora/ui/util/CalculadoraUtil.java
+++ src/main/java/tk/leooresende01/calculadora/ui/util/CalculadoraUtil.java
@@ -3,13 +3,20 @@ package tk.leooresende01.calculadora.ui.util;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class CalculadoraUtil {
-	
+	private static final Double JANELA_LARGURA = 325.0;
+	private static final Double JANELA_ALTURA = 450.0;
+
 	public static void definirCena(String fxmlLocation, Scene cena) {
 		try {
 			Parent load = FXMLLoader.load(CalculadoraUtil.class.getResource(fxmlLocation));
 			cena.setRoot(load);
+			Stage palco = (Stage) cena.getWindow();
+			palco.setResizable(false);
+			palco.setWidth(CalculadoraUtil.JANELA_LARGURA);
+			palco.setHeight(CalculadoraUtil.JANELA_ALTURA);
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
 		}
